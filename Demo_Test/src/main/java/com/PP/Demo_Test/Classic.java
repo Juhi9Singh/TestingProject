0e97892b1156266fa45ddc8a7bd4e1ef821e0a03
Mukteshwar <Mukteshwar@juhi.bbrouter>
2023-08-10 20:19:28 +0530

    commit in NEW on GIT

---
+++ Demo_Test/src/main/java/com/PP/Demo_Test/Classic.java
@@ -0,0 +1,10 @@
+package com.PP.Demo_Test;
+
+public class Classic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+	}
+
+}
